adb52c13666c602068f540b8f3ec9636810d3781
Konstantin Popov <popov.k.g@gmail.com>
2022-08-03 17:21:50 +0300

    done task#1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import { nanoid } from 'nanoid';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -22,30 +22,44 @@ class App extends Component {
 
   removeContact = id => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== id)
+      contacts: prevState.contacts.filter(el => el.id !== id),
     }));
-  }
+  };
 
-  handleFormSubmit = (formData) => {
+  handleFormSubmit = formData => {
     const { contacts } = this.state;
-    return (
-      contacts.find(el => el.name.toLowerCase() === formData.name.toLowerCase())
+    return contacts.find(
+      el => el.name.toLowerCase() === formData.name.toLowerCase()
+    )
       ? alert(formData.name + ' is already in contacts.')
       : this.setState(prev => ({
-        contacts: [
-          ...prev.contacts,
-          { ...formData, id: nanoid()},
-        ],
-      }))
-    )  
-}
+          contacts: [...prev.contacts, { ...formData, id: nanoid() }],
+        }));
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (!contacts) return;
+
+    this.setState({ contacts: contacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
 
     const normalizeContactName = filter.toLowerCase();
 
-    const filteredContacesList = contacts.filter(el => el.name.toLowerCase().includes(normalizeContactName));
+    const filteredContacesList = contacts.filter(el =>
+      el.name.toLowerCase().includes(normalizeContactName)
+    );
 
     return (
       <Container>
@@ -54,7 +68,10 @@ class App extends Component {
 
         <h2>Contacts</h2>
         <Filter filter={filter} handleChange={this.handleChange} />
-        <ContactsList contacts={filteredContacesList} removeContact={ this.removeContact} />
+        <ContactsList
+          contacts={filteredContacesList}
+          removeContact={this.removeContact}
+        />
       </Container>
     );
   }
